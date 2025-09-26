c848dead2d4430f682eb2ddebdb420fb098da1db
Jose Carlos <josecarlos.anton@outlook.com>
2025-09-27 00:48:13 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import Card from "./components/card";
+import Card from "./components/Card";
 
 function App() {
 	const [data, setData] = useState({});
